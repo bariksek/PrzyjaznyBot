c6a883a484c543ee7166888e7e06508e18f0baf7
bariksek <b.sedlak@interia.pl>
2021-07-27 12:34:46 +0200

    Basic program created

---
+++ PrzyjaznyBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using DSharpPlus;
+using System.Threading.Tasks;
+
+namespace PrzyjaznyBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "token",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
