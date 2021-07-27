06fd357bbfc286e78bd23ae9795dcd3651475f4f
bariksek <b.sedlak@interia.pl>
2021-07-27 20:06:53 +0200

    Create new simple commands

--- PrzyjaznyBot/Commands/TestModule.cs
+++ PrzyjaznyBot/Commands/TestModule.cs
@@ -1,15 +1,22 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using System.Threading.Tasks;
 
 namespace PrzyjaznyBot.Commands
 {
     public class TestModule : BaseCommandModule
     {
-        [Command("greet")]
-        public async Task GreetComand(CommandContext ctx)
+        [Command("elo")]
+        public async Task EloComand(CommandContext ctx)
         {
-            await ctx.RespondAsync("Greetings! Thank you for executing me!");
+            await ctx.RespondAsync($"Elo {ctx.Member.Mention} mordo, jak tam zdróweczko?");
+        }
+        
+        [Command("bet")]
+        public async Task BetCommand(CommandContext ctx, int id, int win, int value)
+        {
+            await ctx.RespondAsync($"Ostrożnie! {ctx.Member.Mention} obstawia {value} złotych polskich na status: {win}!");
         }
     }
 }
--- PrzyjaznyBot/Program.cs
+++ PrzyjaznyBot/Program.cs
@@ -16,7 +16,7 @@ namespace PrzyjaznyBot
         {
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "ODY5NDg3Mjc0MTg5MDIxMjE1.YP-7IA.V3paj0TqBb6L19B4_7HkMB6JUps",
+                Token = "token",
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
@@ -27,7 +27,7 @@ namespace PrzyjaznyBot
             });
 
             commands.RegisterCommands<TestModule>();
-
+            
             await discord.ConnectAsync();
             await Task.Delay(-1);
         }
