e0de0c2685e28f8097d454649a819816e6d62f17
sasuuu <fmerta@gmail.com>
2021-09-15 17:35:02 +0200

    Fixes

--- PrzyjaznyBot/Migrations/20210915153856_PrzyjaznyBotDbv2.Designer.cs
+++ PrzyjaznyBot/Migrations/20210915153856_PrzyjaznyBotDbv2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PrzyjaznyBot.DAL;
@@ -9,9 +10,10 @@ using PrzyjaznyBot.DAL;
 namespace PrzyjaznyBot.Migrations
 {
     [DbContext(typeof(PostgreSqlContext))]
-    partial class PostgreSqlContextModelSnapshot : ModelSnapshot
+    [Migration("20210915153856_PrzyjaznyBotDbv2")]
+    partial class PrzyjaznyBotDbv2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
