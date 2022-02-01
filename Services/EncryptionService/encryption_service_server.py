9dfd6b1529e3a3eb5d25e30f23c3ff0a37ec3454
sasuuu <fmerta@gmail.com>
2022-02-02 00:57:26 +0100

    Add key fetching from database

--- Services/EncryptionService/encryption_service_server.py
+++ Services/EncryptionService/encryption_service_server.py
@@ -1,13 +1,16 @@
+import configparser
 import grpc
 import encryption_service_pb2
 import encryption_service_pb2_grpc
 from cryptography.fernet import Fernet
 from concurrent import futures
+import pathlib
+import argparse
 
 class EncryptionServicer(encryption_service_pb2_grpc.EncryptionServiceServicer):
 
-    def __init__(self):
-        self.fernet = Fernet('fLiXGGc_sWtwM9Oj94UdWPg2oJ81ryDoLAPIzVglS-8=')
+    def __init__(self, key):
+        self.fernet = Fernet(key)
 
     def Encrypt(self, request, context):
         cipher = self.fernet.encrypt(request.message.encode())
@@ -17,12 +20,38 @@ class EncryptionServicer(encryption_service_pb2_grpc.EncryptionServiceServicer):
         message = self.fernet.decrypt(request.cipher.encode()).decode()
         return encryption_service_pb2.DecryptResponse(message=message)
 
-def serve():
+def serve(key):
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=5))
-    encryption_service_pb2_grpc.add_EncryptionServiceServicer_to_server(EncryptionServicer(), server)
+    encryption_service_pb2_grpc.add_EncryptionServiceServicer_to_server(EncryptionServicer(key), server)
     server.add_insecure_port('[::]:50051')
     server.start()
     server.wait_for_termination()
 
+def get_key():
+    config = configparser.ConfigParser()
+    config.read('{}/database.ini'.format(pathlib.Path(__file__).parent.resolve()))
+    if 'postgresql' not in config:
+        raise Exception('Missing postgresql configuration')
+    connection = None
+    key = ''
+    try:
+        connection = psycopg2.connect(host=config['host'], database=config['database'], user=config['user'], password=config['password'])
+        cursor = connection.cursor()
+        cursor.execute("SELECT key_id, key FROM keys ORDER BY key_id")
+        row = cursor.fetchone()
+        key = row[1]
+    except:
+        print('There was an issue during key fetching')
+    finally:
+        if connection is not None:
+            connection.close()
+        return key
+        
+
 if __name__ == '__main__':
-    serve()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    print('Debug mode: {}'.format(args.debug))
+    key = Fernet.generate_key() if args.debug else get_key()
+    serve(key)
