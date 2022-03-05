9d40816c4d8b7d009e6f197a2134a267dcef1d14
sasuuu <fmerta@gmail.com>
2022-03-05 16:40:39 +0100

    Add mappers

---
+++ Services/BetService/Mappers/BetMapper.cs
@@ -0,0 +1,21 @@
+﻿using Google.Protobuf.WellKnownTypes;
+
+namespace BetService.Mappers
+{
+    public static class BetMapper
+    {
+        public static Bet Map(this Model.Bet bet)
+        {
+            return new Bet
+            {
+                AuthorId = bet.AuthorId,
+                Id = bet.Id,
+                IsActive = bet.IsActive,
+                IsStopped = bet.IsStopped,
+                Message = bet.Message,
+                Stake = bet.Stake,
+                DateTime = DateTime.SpecifyKind(bet.DateTime, DateTimeKind.Utc).ToTimestamp()
+            };
+        }
+    }
+}
---
+++ Services/BetService/Mappers/UserBetMapper.cs
@@ -0,0 +1,28 @@
+﻿namespace BetService.Mappers
+{
+    public static class UserBetMapper
+    {
+        public static UserBet Map(this Model.UserBet userBet)
+        {
+            return new UserBet
+            {
+                BetId = userBet.BetId,
+                Id = userBet.Id,
+                IsActive = userBet.IsActive,
+                IsFinished = userBet.IsFinished,
+                UserId = userBet.UserId,
+                Condition = MapCondition(userBet.Condition)
+            };
+        }
+
+        private static Condition MapCondition(Common.Condition condition)
+        {
+            return condition switch
+            {
+                Common.Condition.Yes => Condition.Yes,
+                Common.Condition.No => Condition.No,
+                _ => Condition.No,
+            };
+        }
+    }
+}
